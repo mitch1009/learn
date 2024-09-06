98b7a64030d2e216d84466364bb5cca1eb8c0c02
Mitch Chanza <mitch.chanza@gmail.com>
2024-09-06 11:56:10 +0200

    create 24 files and update 2 files

---
+++ frontend/nextapp/src/components/HomeModule/HomeModule.tsx
@@ -0,0 +1,6 @@
+
+export const HomeModule = () => {
+    return (
+        <div>HomeModule</div>
+    )
+}
---
+++ frontend/nextapp/src/components/HomeModule/TopNav/TopNavComponent.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const TopNavComponent = () => {
+    return (
+        <div>TopNavComponent</div>
+    )
+}
